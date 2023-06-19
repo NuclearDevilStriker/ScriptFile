715bca4b1488608e71b90892152b068d35a26f72
DexterEskalarte <136956225+PiercingDoll@users.noreply.github.com>
2023-06-20 07:11:34 +0800

    Update try.sh

--- try.sh
+++ try.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-fun_bar () {
-comando[0]="$1"
-comando[1]="$2"
- (
-[[ -e $HOME/fim ]] && rm $HOME/fim
-${comando[0]} -y > /dev/null 2>&1
-${comando[1]} -y > /dev/null 2>&1
-touch $HOME/fim
- ) > /dev/null 2>&1 &
- tput civis
-echo -ne "\033[1;33m["
-while true; do
-   for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m#"
-   sleep 0.1s
-   done
-   [[ -e $HOME/fim ]] && rm $HOME/fim && break
-   echo -e "\033[1;33m]"
-   sleep 1s
-   tput cuu1
-   tput dl1
-   echo -ne "\033[1;33m["
-done
-echo -e "\033[1;33m]\033[1;37m -\033[1;32m SUCCESS INSTALLED!!\033[1;37m"
-tput cnorm
-}
-
-RED='\033[01;31m';
-RESET='\033[0m';
-GREEN='\033[01;32m';
-WHITE='\033[01;37m';
-YELLOW='\033[00;33m';
 
 echo -e "\033[1;31m═══════════════════════════════════════════════════\033[0m"
 echo '                                                                  
@@ -47,8 +15,7 @@ echo -e "\033[1;31m════════════════════�
 echo -e "\033[1;31mStarting Script Withiin 5 Seconds\033[0m"
 sleep 5
   
- 
-function obfs() { 
+
 read -rp "Please Enter Script Password: " obfs
 if [ $obfs == '272124' ]
 then
@@ -56,7 +23,7 @@ then
 read -rp "Please Enter Script Password: " password
 if [ $password == '272124' ]
 then
-}
+
 
 install_hysteria(){
 clear
@@ -301,7 +268,7 @@ sed -i "s|SERVERIP|$server_ip|g" /etc/.counter
   }&>/dev/null
 }
 
-obfs
+
 install_hysteria
 create_hostname
 install_firewall_kvm
