35e93088574d0cd688b4164653fcb9575c15bb66
DexterEskalarte <136956225+PiercingDoll@users.noreply.github.com>
2023-06-25 11:37:06 +0800

    Update and rename aaa.sh to aaaa.sh

---
+++ aaaa.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+function hyteria_port() {
+  read -rp "Please enter the port number (default：5666)：" UDP_PORT
+  [ -z "$UDP_PORT" ] && UDP_PORT="5666"
+  if [[ $UDP_PORT -le 0 ]] || [[ $UDP_PORT -gt 65535 ]]; then
+    print_error "Please enter a value between 0-65535"
+    exit 1
+  fi
+ }
+ 
+ function hyteria_domain() {
+  read -rp "Please enter your domain (example：dexterpogi.mediatek.xyz)：" DOMAIN
+  [ -z "$DOMAIN" ] && DOMAIN="dexterpogi.mediatek.xyz"
+  if [[ $DOMAIN -le 0 ]] || [[ $DOMAIN -gt 65535 ]]; then
+    print_error "Please enter a value between 0-65535"
+    exit 1
+  fi
+ }
+ 
+ function hyteria_obfs() {
+  read -rp "Please enter your OBFS (example：mediatekvpn)：" OBFS
+  [ -z "$OBFS" ] && OBFS="mediatekvpn"
+  if [[ $OBFS -le 0 ]] || [[ $OBFS -gt 65535 ]]; then
+    print_error "Please enter a value between 0-65535"
+    exit 1
+  fi
+ }
+ 
+ function hyteria_password() {
+  read -rp "Please enter your paasword (example：dexterpogi：" PASSWORD
+  [ -z "$PASSWORD" ] && PASSWORD="dexterpogi"
+  if [[ $PASSWORD -le 0 ]] || [[ $PASSWORD -gt 65535 ]]; then
+    print_error "Please enter a value between 0-65535"
+    exit 1
+  fi
+ }
+
+
+
+
+modify_port
+hyteria_domain
+hyteria_obfs
+hyteria_password
+ 
+echo -e " \e[92m Hysteria Port:\e[0m \e[97m: $UDP_PORT\e[0m"
+echo -e " \e[92m Hysteria Domain:\e[0m \e[97m: $DOMAIN\e[0m"
+echo -e " \e[92m Hysteria Obfs:\e[0m \e[97m: $OBFS\e[0m"
+echo -e " \e[92m Hysteria Password:\e[0m \e[97m: $PASSWORD\e[0m"
+  
