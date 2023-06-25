a541ca52cceef7f3ae940b3912461bf9c5f76da6
DexterEskalarte <136956225+PiercingDoll@users.noreply.github.com>
2023-06-25 11:21:15 +0800

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+function modify_port() {
+  read -rp "Please enter the port number (default：5666)：" UDP_PORT
+  [ -z "$UDP_PORT" ] && UDP_PORT="5666"
+  if [[ $UDP_PORT -le 0 ]] || [[ $UDP_PORT -gt 65535 ]]; then
+    print_error "Please enter a value between 0-65535"
+    exit 1
+  fi
+ }
+
+ function modify_port
+ 
+ echo -e " \e[92m Hysteria Port:\e[0m \e[97m: $DOMAIN\e[0m"
+  
